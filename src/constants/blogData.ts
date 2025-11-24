8a69fe4fad039825c317d9fcd8a09f6392ec51fa
logico <43566742+logik101@users.noreply.github.com>
2025-11-24 12:49:46 +0700

    Auto-update via MicroFormS Dashboard

---
+++ src/constants/blogData.ts
@@ -0,0 +1,46 @@
+import { BlogArticle } from '@/types';
+
+export const blogArticles: BlogArticle[] = [
+    {
+        "id": 4,
+        "titleKey": "vbbb",
+        "descriptionKey": "asad",
+        "contentKey": "saafa",
+        "imageUrl": "https://via.placeholder.com/800x400",
+        "date": "11/24/2025",
+        "author": "admin",
+        "readTimeKey": "1",
+        "tagsKey": "Community",
+        "isHidden": false
+    },
+    {
+        "id": 1,
+        "imageUrl": "https://logik101.github.io/microF/b1.avif",
+        "titleKey": "blogTitleAi",
+        "descriptionKey": "blogDescAi",
+        "contentKey": "blogContentAi",
+        "tagsKey": "blogTagsAI",
+        "date": "4/5/2025",
+        "readTimeKey": "readTime5min"
+    },
+    {
+        "id": 2,
+        "imageUrl": "https://logik101.github.io/microF/b2.avif",
+        "titleKey": "blogTitlePioneer",
+        "descriptionKey": "blogDescPioneer",
+        "contentKey": "blogDescPioneer",
+        "tagsKey": "blogTagsExpertise",
+        "date": "4/3/2025",
+        "readTimeKey": "readTime1min"
+    },
+    {
+        "id": 3,
+        "imageUrl": "https://logik101.github.io/microF/b3.avif",
+        "titleKey": "blogTitleInnovation",
+        "descriptionKey": "blogDescInnovation",
+        "contentKey": "blogDescInnovation",
+        "tagsKey": "blogTagsExpertise",
+        "date": "3/8/2025",
+        "readTimeKey": "readTime1min"
+    }
+];
