c51c506f9bb2ca08ff721a97eb63a647daf335e8
logico <43566742+logik101@users.noreply.github.com>
2025-12-01 00:30:27 +0700

    Auto-update via MicroFormS Dashboard

--- src/constants/blogData.ts
+++ src/constants/blogData.ts
@@ -2,6 +2,18 @@ import { BlogArticle } from '@/types';
 
 export const blogArticles: BlogArticle[] = [
     {
+        "id": 5,
+        "titleKey": "The Blog",
+        "descriptionKey": "A&nbsp;<span class=\"Yjhzub\" jscontroller=\"VhkxAe\" jsuid=\"bNYCef_c\" data-complete=\"true\" data-processed=\"true\" style=\"font-weight: 700; color: rgb(10, 10, 10); font-family: &quot;Google Sans&quot;, Roboto, Arial, sans-serif; font-size: 16px;\">blog</span><span style=\"color: rgb(10, 10, 10); font-family: &quot;Google Sans&quot;, Roboto, Arial, sans-serif; font-size: 16px;\">&nbsp;(short for \"weblog\") is&nbsp;</span><mark class=\"HxTRcb\" jscontroller=\"DfH0l\" jsuid=\"bNYCef_d\" data-complete=\"true\" data-sae=\"\" style=\"color: rgb(10, 10, 10); border-radius: 4px; background: none 0% 0% / auto repeat scroll padding-box border-box rgb(211, 227, 253); padding: 0px 2px; font-family: &quot;Google Sans&quot;, Roboto, Arial, sans-serif; font-size: 16px;\">an online platform or website that publishes regularly updated content, known as posts, in a reverse chronological order</mark><span style=\"color: rgb(10, 10, 10); font-family: &quot;Google Sans&quot;, Roboto, Arial, sans-serif; font-size: 16px;\">. It is essentially an online journal or informational website that can be run by an individual, group, or business.</span>",
+        "contentKey": "A&nbsp;<span class=\"Yjhzub\" jscontroller=\"VhkxAe\" jsuid=\"bNYCef_c\" data-complete=\"true\" data-processed=\"true\" style=\"font-weight: 700; color: rgb(10, 10, 10); font-family: &quot;Google Sans&quot;, Roboto, Arial, sans-serif; font-size: 16px;\">blog</span><span style=\"color: rgb(10, 10, 10); font-family: &quot;Google Sans&quot;, Roboto, Arial, sans-serif; font-size: 16px;\">&nbsp;(short for \"weblog\") is&nbsp;</span><mark class=\"HxTRcb\" jscontroller=\"DfH0l\" jsuid=\"bNYCef_d\" data-complete=\"true\" data-sae=\"\" style=\"color: rgb(10, 10, 10); border-radius: 4px; background: none 0% 0% / auto repeat scroll padding-box border-box rgb(211, 227, 253); padding: 0px 2px; font-family: &quot;Google Sans&quot;, Roboto, Arial, sans-serif; font-size: 16px;\">an online platform or website that publishes regularly updated content, known as posts, in a reverse chronological order</mark><span style=\"color: rgb(10, 10, 10); font-family: &quot;Google Sans&quot;, Roboto, Arial, sans-serif; font-size: 16px;\">. It is essentially an online journal or informational website that can be run by an individual, group, or business.</span>",
+        "imageUrl": "https://via.placeholder.com/800x400",
+        "date": "11/30/2025",
+        "author": "Admin",
+        "readTimeKey": "5 min read",
+        "tagsKey": "Community",
+        "isHidden": false
+    },
+    {
         "id": 4,
         "titleKey": "This is a test",
         "descriptionKey": "This is a test",
